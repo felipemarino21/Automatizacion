6f556bd56a53df1129ba65112a9cf6fba62e7215
felipemarino <luisfelipemarino@gmail.com>
2022-07-02 16:21:44 -0500

    first commit

---
+++ src/main/java/io/cucumber/skeleton/test.java
@@ -0,0 +1,5 @@
+package io.cucumber.skeleton;
+
+
+public class test {
+}
---
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -0,0 +1,18 @@
+package io.cucumber.skeleton;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/cucumber/skeleton")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
+//@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@Test")
+public class RunCucumberTest {
+}
